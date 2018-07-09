904bac4ca0b84f60a40cdf0a6fe2439308e69ac2
jeevkulk <jeevanakulkarni@gmail.com>
2018-07-09 19:32:56 +0530

    Corrected behaviour of print odd / even numbers

--- src/main/java/concurrency/executor/PrintOddEvenNumbers.java
+++ src/main/java/concurrency/executor/PrintOddEvenNumbers.java
@@ -2,11 +2,16 @@ package concurrency.executor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class PrintOddEvenNumbers {
 
     volatile int counter = 0;
+    int maxCount = 25;
+    Object object = new Object();
 
     public static void main(String[] args) {
         PrintOddEvenNumbers printOddEvenNumbers = new PrintOddEvenNumbers();
@@ -15,20 +20,44 @@ public class PrintOddEvenNumbers {
 
     public void process() {
         Callable<Integer> printOddNumbersTask = () -> {
-            if (counter % 2 != 0)
-                System.out.println(Thread.currentThread() + " : " + counter++);
+            try {
+                while (counter < maxCount) {
+                    synchronized (object) {
+                        if (counter % 2 != 0) {
+                            System.out.println(Thread.currentThread() + " : " + counter++);
+                            object.notify();
+                            if (counter < maxCount)
+                                object.wait();
+                        }
+                    }
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Caught ="+ex.getMessage());
+                ex.printStackTrace();
+            }
             return counter;
         };
         Callable<Integer> printEvenNumbersTask = () -> {
-            if (counter % 2 == 0)
-                System.out.println(Thread.currentThread() + " : " + counter++);
+            try {
+                while (counter < maxCount) {
+                    synchronized (object) {
+                        if (counter % 2 == 0) {
+                            System.out.println(Thread.currentThread() + " : " + counter++);
+                            object.notify();
+                            if (counter < maxCount)
+                                object.wait();
+                        }
+                    }
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Caught ="+ex.getMessage());
+                ex.printStackTrace();
+            }
             return counter;
         };
-        List<Callable<Integer>> tasks = new ArrayList<>(200);
-        for (int i = 0; i < 100; i ++) {
-            tasks.add(printOddNumbersTask);
-            tasks.add(printEvenNumbersTask);
-        }
+        List<Callable<Integer>> tasks = new ArrayList<>(2);
+        tasks.add(printEvenNumbersTask);
+        tasks.add(printOddNumbersTask);
         ExecutorService service = Executors.newFixedThreadPool(2);
         try {
             List<Future<Integer>> futures = service.invokeAll(tasks);
